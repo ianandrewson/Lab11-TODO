4aec472ec9be14ff434c6ddc7331e2311d3f6f6f
Ian Andrewson <ianandrewson@gmail.com>
2019-11-15 17:49:20 -0800

    route to add user works

--- server.js
+++ server.js
@@ -1,6 +1,32 @@
 // Load Environment Variables from the .env file
 require('dotenv').config();
 
+const ensureAuth = require('./lib/auth/ensure-auth');
+const createAuthRoutes = require('./lib/auth/create-auth-routes');
+const authRoutes = createAuthRoutes({
+    selectUser(email) {
+        return client.query(`
+            SELECT id, hash
+            FROM users
+            where email = $1;
+        `,
+        [email]
+        ).then(result => result.rows[0]);
+    },
+    insertUser(user, hash) {
+        console.log(user);
+        return client.query(`
+            INSERT into users (email, hash)
+            VALUES ($1, $2)
+            RETURNING id, email;
+        `,
+        [user.email, hash]
+        ).then(result => result.rows[0]);
+    }
+});
+
+
+
 // Application Dependencies
 const express = require('express');
 const cors = require('cors');
@@ -16,6 +42,8 @@ app.use(morgan('dev')); // http logging
 app.use(cors()); // enable CORS request
 app.use(express.static('public')); // server files from /public folder
 app.use(express.json()); // enable reading incoming json data
+app.use('/api/auth', authRoutes);
+app.use('/api', ensureAuth);
 
 // API Routes
 
